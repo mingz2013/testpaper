fc755c5b5140648294ac26832dd31a99a7256df6
mingz <305603665@qq.com>
2016-12-17 18:41:06 +0800

    init

--- testpaper.js
+++ testpaper.js
@@ -5,126 +5,180 @@
 
 
 
-var page_main = function (main, num, danxuanItemsAll, duoxuanItemsAll, panduanItemsAll) {
+var page_main = function (num, danxuanItemsAll, duoxuanItemsAll, panduanItemsAll) {
 
-    var getRandomArray = function (arr) {
-        var len = arr.length;
-        if (len < num) {
-            return arr;
-        }
-        var result_index = [];
-        while (result_index.length < num) {
-            var r = Math.floor(Math.random() * arr.length);
-            if (result_index.indexOf(r) == -1) {
-                result_index.push(r);
-            }
-        }
-        result_index.sort();
-        console.log(result_index);
-        var result = [];
-        result_index.forEach(function (i) {
-            result.push(arr[i]);
-        });
-        return result;
-    };
-
-    var getDanxuanItemView = function (item, index) {
-        var l = item.split(',');
-        var result = "<div>" +
-            "<p><span>" + index + "<span><span>" + l[1] + "</span></p>" +
-            "<p>" +
-            "<label><input name='" + l[0] + "' type='radio' value='A' />" + l[2] + "</label>" +
-            "<label><input name='" + l[0] + "' type='radio' value='B' />" + l[3] + "</label>" +
-            "<label><input name='" + l[0] + "' type='radio' value='C' />" + l[4] + "</label>" +
-            "<label><input name='" + l[0] + "' type='radio' value='D' />" + l[5] + "</label>" +
-            "</p>" +
-            "</div>";
-        return result;
-    };
-
-    var getDuoxuanItemView = function (item, index) {
-        var l = item.split(',');
-        var result = "<div>" +
-            "<p><span>" + index + "<span><span>" + l[1] + "</span></p>" +
-            "<p>" +
-            "<label><input name='" + l[0] + "' type='checkbox' value='A' />" + l[2] + "</label>" +
-            "<label><input name='" + l[0] + "' type='checkbox' value='B' />" + l[3] + "</label>" +
-            "<label><input name='" + l[0] + "' type='checkbox' value='C' />" + l[4] + "</label>" +
-            "<label><input name='" + l[0] + "' type='checkbox' value='D' />" + l[5] + "</label>" +
-            "</p>" +
-            "</div>";
-        return result;
-    };
-    var getPanduanItemView = function (item, index) {
-        var l = item.split(',');
-        var result = "<div>" +
-            "<p><span>" + index + "<span><span>" + l[1] + "</span></p>" +
-            "<p>" +
-            "<label><input name='" + l[0] + "' type='radio' value='错' />错</label>" +
-            "<label><input name='" + l[0] + "' type='radio' value='对' />对</label>" +
-            "</p>" +
-            "</div>";
-        return result;
-    };
-
-    var getDanxuanListView = function (items) {
-        var result = "<h2>单选  （20个 每个两分）</h2>";
-
-        items.forEach(function (item, index) {
-            result += getDanxuanItemView(item, index);
-        });
-        return result;
-    };
-    var getDuoxuanListView = function (items) {
-        var result = "<h2>多选（20个 每个两分）</h2>";
-
-        items.forEach(function (item, index) {
-            result += getDuoxuanItemView(item, index);
-        });
-        return result;
-    };
-    var getPanduanListView = function (items) {
-        var result = "<h2>判断（20个 每个一分）</h2>";
-
-        items.forEach(function (item, index) {
-            result += getPanduanItemView(item, index);
-        });
-        return result;
-    };
-
-    var getLeftView = function () {
-        var result = "<div>";
-        result += getDanxuanListView(danxuanItems);
-        result += getDuoxuanListView(duoxuanItems);
-        result += getPanduanListView(panduanItems);
-        result += "</div>";
-        return result;
-    };
-
-    var getRightView = function () {
-        var result = "<div>计时: <span id='time'></span></div>";
-        return result;
-    };
 
     var getView = function () {
+        var getDanxuanItemView = function (item, index) {
+            var l = item.split(',');
+            var result = "<div>" +
+                "<p><span>" + index + "<span><span>" + l[1] + "</span></p>" +
+                "<p>" +
+                "<label><input name='" + l[0] + "' type='radio' value='A' />" + l[2] + "</label>" +
+                "<label><input name='" + l[0] + "' type='radio' value='B' />" + l[3] + "</label>" +
+                "<label><input name='" + l[0] + "' type='radio' value='C' />" + l[4] + "</label>" +
+                "<label><input name='" + l[0] + "' type='radio' value='D' />" + l[5] + "</label>" +
+                "</p>" +
+                "</div>";
+            return result;
+        };
+
+        var getDuoxuanItemView = function (item, index) {
+            var l = item.split(',');
+            var result = "<div>" +
+                "<p><span>" + index + "<span><span>" + l[1] + "</span></p>" +
+                "<p>" +
+                "<label><input name='" + l[0] + "' type='checkbox' value='A' />" + l[2] + "</label>" +
+                "<label><input name='" + l[0] + "' type='checkbox' value='B' />" + l[3] + "</label>" +
+                "<label><input name='" + l[0] + "' type='checkbox' value='C' />" + l[4] + "</label>" +
+                "<label><input name='" + l[0] + "' type='checkbox' value='D' />" + l[5] + "</label>" +
+                "</p>" +
+                "</div>";
+            return result;
+        };
+        var getPanduanItemView = function (item, index) {
+            var l = item.split(',');
+            var result = "<div>" +
+                "<p><span>" + index + "<span><span>" + l[1] + "</span></p>" +
+                "<p>" +
+                "<label><input name='" + l[0] + "' type='radio' value='错' />错</label>" +
+                "<label><input name='" + l[0] + "' type='radio' value='对' />对</label>" +
+                "</p>" +
+                "</div>";
+            return result;
+        };
+
+        var getDanxuanListView = function (items) {
+            var result = "<h2>单选  （20个 每个两分）</h2>";
+
+            items.forEach(function (item, index) {
+                result += getDanxuanItemView(item, index);
+            });
+            return result;
+        };
+        var getDuoxuanListView = function (items) {
+            var result = "<h2>多选（20个 每个两分）</h2>";
+
+            items.forEach(function (item, index) {
+                result += getDuoxuanItemView(item, index);
+            });
+            return result;
+        };
+        var getPanduanListView = function (items) {
+            var result = "<h2>判断（20个 每个一分）</h2>";
+
+            items.forEach(function (item, index) {
+                result += getPanduanItemView(item, index);
+            });
+            return result;
+        };
+
+        var getLeftView = function () {
+            var result = "<div>";
+            result += getDanxuanListView(danxuanItems);
+            result += getDuoxuanListView(duoxuanItems);
+            result += getPanduanListView(panduanItems);
+            result += "</div>";
+            return result;
+        };
+
+        var getRightView = function () {
+            var result = "<div>计时: <span id='time'></span></div>";
+            return result;
+        };
+
         var result = "";
         result += getRightView();
         result += getLeftView();
-        result += "<button>提交</button>";
+        result += "<button id='btn'>提交</button>";
         return result;
     };
 
 
     var randomData = function () {
+        var getRandomArray = function (arr) {
+            var len = arr.length;
+            if (len < num) {
+                return arr;
+            }
+            var result_index = [];
+            while (result_index.length < num) {
+                var r = Math.floor(Math.random() * arr.length);
+                if (result_index.indexOf(r) == -1) {
+                    result_index.push(r);
+                }
+            }
+            result_index.sort();
+            console.log(result_index);
+            var result = [];
+            result_index.forEach(function (i) {
+                result.push(arr[i]);
+            });
+            return result;
+        };
+
         danxuanItems = getRandomArray(danxuanItemsAll);
         duoxuanItems = getRandomArray(duoxuanItemsAll);
         panduanItems = getRandomArray(panduanItemsAll);
     };
 
+    var getScore = function () {
+
+        var getDanxuanScore = function () {
+            var score = 0;
+            danxuanItems.forEach(function (item) {
+                var l = item.split(',');
+                var number = l[0];
+                var right = l[6];
+                var val = document.getElementsByName(number).valueOf();
+                if (val == right) {
+                    score += 2;
+                }
+            });
+            return score;
+        };
+        var getDuoxuanScore = function () {
+            var score = 0;
+            danxuanItems.forEach(function (item) {
+                var l = item.split(',');
+                var number = l[0];
+                var right = l[6];
+                var val = document.getElementsByName(number).valueOf();
+                if (right.indexOf(val) > -1) {
+                    score += 2;
+                }
+            });
+            return score;
+        };
+        var getPanduanScore = function () {
+            var score = 0;
+            danxuanItems.forEach(function (item) {
+                var l = item.split(',');
+                var number = l[0];
+                var right = l[2];
+                var val = document.getElementsByName(number).valueOf();
+                if (val == right) {
+                    score += 1;
+                }
+            });
+            return score;
+        };
+
+        return getDanxuanScore() + getDuoxuanScore() + getPanduanScore();
+    };
+
 
     var init = function () {
+
         randomData();
-        main.innerHTML = getView();
+
+        document.getElementById('main').innerHTML = getView();
+
+        document.getElementById('btn').onclick = function () {
+            var score = getScore();
+            page_result(score);
+        };
+
     };
 
     var danxuanItems, duoxuanItems, panduanItems = [];
@@ -132,6 +186,23 @@ var page_main = function (main, num, danxuanItemsAll, duoxuanItemsAll, panduanIt
 
 };
 
+
+var page_result = function (score) {
+    var getView = function (score) {
+        var result = "<div><p>得分:" + score + "</p><p><button id='btn'>再来一次</button></p></div>"
+        return result;
+    };
+
+    var init = function () {
+        document.getElementById('main').innerHTML = getView();
+
+        document.getElementById('btn').onclick = function () {
+            location.reload();
+        };
+    };
+
+    init();
+};
 
 window.onload = function () {
 
@@ -145,9 +216,8 @@ window.onload = function () {
 
     var num = 10;
 
-    var main = document.getElementById('main');
 
-    page_main(main, num, danxuanItemsAll, duoxuanItemsAll, panduanItemsAll);
+    page_main(num, danxuanItemsAll, duoxuanItemsAll, panduanItemsAll);
 
 
 };
